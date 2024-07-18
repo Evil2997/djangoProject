66a7a25ac1f13bba8747b48ed35312a41ed47f50
Evil2997 <dima.molchanow2012@gmail.com>
2024-07-18 17:58:32 +0300

    Add Book pages in cycle for

--- app1/urls.py
+++ app1/urls.py
@@ -3,6 +3,7 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('about/', views.about),
-    path('books/', views.books),
+    path('books/', views.book_list, name='book_list'),
+    path('books/<int:id>/', views.book_detail, name='book_detail'),
 
 ]
