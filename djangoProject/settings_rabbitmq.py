cb2e30a2d34ade2baf51f54b72ccbb9afadd3f05
Evil2997 <dima.molchanow2012@gmail.com>
2024-08-12 13:41:15 +0300

    Add logging

--- djangoProject/settings_rabbitmq.py
+++ djangoProject/settings_rabbitmq.py
@@ -2,3 +2,31 @@ from .settings import *
 
 CELERY_BROKER_URL = 'amqp://guest:guest@rabbitmq:5672//'
 CELERY_RESULT_BACKEND = 'rpc://'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+        'file': {
+            'class': 'logging.FileHandler',
+            'filename': BASE_DIR / 'logfile.log',  # Файл лога будет в корне вашего проекта
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',
+        },
+        'celery': {
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',  # Если логи не выводятся, попробуй уровень DEBUG / INFO
+        },
+        'app2': {
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',
+        },
+    },
+}
